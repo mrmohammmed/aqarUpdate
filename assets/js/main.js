976024af4621dce7b99f62619fc15922f6b958fe
mohamed ahmed <mrmohammedahmed43@gmail.com>
2024-09-30 03:08:36 +0300

    edit at slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var swiper = new Swiper('.swiper-container', {
-      loop: true, // Enable loop mode
-      navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-      },
-      slidesPerView: 3, // Show 3 slides at a time
-      spaceBetween: 20, // Space between slides
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    loop: true, // Enable loop mode
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 3, // Show 3 slides at a time
+    spaceBetween: 20, // Space between slides
   });
 });
