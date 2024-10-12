83fa1ae6231d8fed89e54fb61f5a84e0bf5fee94
mohamed ahmed <mrmohammedahmed43@gmail.com>
2024-10-12 14:22:34 +0300

    side-menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,24 @@ document.addEventListener("DOMContentLoaded", function () {
     spaceBetween: 20, // Space between slides
   });
 });
+// When the link is clicked, show the sidebar
+document
+  .querySelector(".link-style-small2")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".bodySidebar").classList.add("show");
+  });
+
+// When the close button is clicked, hide the sidebar
+document
+  .querySelector(".close-services")
+  .addEventListener("click", function () {
+    document.querySelector(".bodySidebar").classList.remove("show");
+  });
+
+// When clicking outside the side-menu, hide the sidebar
+document.querySelector(".bodySidebar").addEventListener("click", function (e) {
+  if (!e.target.closest(".side-menu")) {
+    document.querySelector(".bodySidebar").classList.remove("show");
+  }
+});
