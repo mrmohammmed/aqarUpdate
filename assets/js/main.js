b8cbed03749c27287785c52383d4eac5b53d915d
mohamed ahmed <mrmohammedahmed43@gmail.com>
2024-10-21 03:05:12 +0300

    validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,37 @@ document.querySelector(".bodySidebar").addEventListener("click", function (e) {
     document.querySelector(".bodySidebar").classList.remove("show");
   }
 });
+
+// Validation for phone number
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    const phone = document.getElementById("phone").value;
+    const phoneRegex = /^05\d{8}$/;
+    const phoneError = document.getElementById("phoneError");
+
+    // Reset error message
+    phoneError.textContent = "";
+
+    if (!phoneRegex.test(phone)) {
+      event.preventDefault(); // Prevent form submission
+      phoneError.textContent = "رقم الجوال يجب أن يكون 10 أرقام ويبدأ بـ 05.";
+    }
+  });
+
+// Validation for national identity number in modal
+document
+  .getElementById("nationalLoginForm")
+  .addEventListener("submit", function (event) {
+    const identityNumber = document.getElementById("identityNumber").value;
+    const identityError = document.getElementById("identityError");
+
+    // Reset error message
+    identityError.textContent = "";
+
+    if (identityNumber.length !== 10 || isNaN(identityNumber)) {
+      event.preventDefault(); // Prevent form submission
+      identityError.textContent =
+        "الرقم الذي أدخلته غير صحيح، تأكد من رقم الهوية.";
+    }
+  });
